307bb98909170cb9b497bedbd5c8466b0bb21b1e
Apocollis <hughesac14@natlcollege.edu>
2016-06-08 00:32:23 -0400

    new file:   src/main/java/com/github/apocollis/infinityrising/InfinityRising.java 	new file:   src/main/java/com/github/apocollis/infinityrising/help/Reference.java 	new file:   src/main/resources/mcmod.info

---
+++ src/main/java/com/github/apocollis/infinityrising/InfinityRising.java
@@ -0,0 +1,31 @@
+package com.github.apocollis.infinityrising;
+
+import com.github.apocollis.infinityrising.help.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class InfinityRising 
+{
+
+	@Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
---
+++ src/main/java/com/github/apocollis/infinityrising/help/Reference.java
@@ -0,0 +1,8 @@
+package com.github.apocollis.infinityrising.help;
+
+public class Reference 
+{
+    public static final String MODID = "com.github.apocollis.infinityrising";
+    public static final String NAME = "InfinityRising";
+    public static final String VERSION = "a.1";	
+}
