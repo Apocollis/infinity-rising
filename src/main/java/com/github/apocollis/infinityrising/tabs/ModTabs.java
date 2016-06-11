fe7fecd4038f7062050be54085ee2c61ff758ad0
Apocollis <hughesac14@natlcollege.edu>
2016-06-10 20:07:33 -0400

    commit 6/10/16

--- src/main/java/com/github/apocollis/infinityrising/tabs/ModTabs.java
+++
@@ -1,8 +0,0 @@
-package com.github.apocollis.infinityrising.tabs;
-
-import net.minecraft.creativetab.CreativeTabs;
-
-public class ModTabs
-{
-	public static CreativeTabs tabInfinityRising = new InfinityRisingTab(CreativeTabs.getNextID(),"tabInfinityRising");
-}
