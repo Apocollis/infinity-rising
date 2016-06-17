425d6200d2676e8ac7b423b146a879d5886e5cc5
Apocollis <hughesac14@natlcollege.edu>
2016-06-17 02:44:04 -0400

    modified:   src/main/java/com/github/apocollis/infinityrising/crafting/ModRecipes.java

--- src/main/java/com/github/apocollis/infinityrising/crafting/ModRecipes.java
+++ src/main/java/com/github/apocollis/infinityrising/crafting/ModRecipes.java
@@ -1,5 +1,6 @@
 package com.github.apocollis.infinityrising.crafting;
 
+import com.github.apocollis.infinityrising.init.ModArmory;
 import com.github.apocollis.infinityrising.init.ModBlocks;
 import com.github.apocollis.infinityrising.init.ModItems;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -14,5 +15,9 @@ public class ModRecipes
 		GameRegistry.addShapedRecipe(new ItemStack(ModItems.boneIngot), new Object[] {" A ", "AAA", " A ", 'A', Items.bone});
 		GameRegistry.addShapelessRecipe(new ItemStack(Items.flint), Blocks.gravel);
 		GameRegistry.addSmelting(ModBlocks.iridiumOre, new ItemStack(ModItems.iridiumIngot),1.2F);
+		GameRegistry.addRecipe(new ItemStack (ModArmory.iridiumPick), new Object[] {"AAA", " B ", " B ", 'A', ModItems.iridiumIngot, 'B', Blocks.obsidian});
+		GameRegistry.addRecipe(new ItemStack (ModArmory.iridiumAxe), new Object[] {" AA", " BA", " B ", 'A', ModItems.iridiumIngot, 'B', Blocks.obsidian});
+		GameRegistry.addRecipe(new ItemStack (ModArmory.iridiumShovel), new Object[] {" A ", " B ", " B ", 'A', ModItems.iridiumIngot, 'B', Blocks.obsidian});
+		GameRegistry.addRecipe(new ItemStack (ModArmory.iridiumSword), new Object[] {" A ", " A ", " B ", 'A', ModItems.iridiumIngot, 'B', Blocks.obsidian});
 	}
 }
